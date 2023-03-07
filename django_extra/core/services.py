70dbec26bcbcb2b504bc26cc4abba944b8a738d3
Abhishek Sharma <abhishm20@gmail.com>
2023-03-07 10:04:02 +0530

    cache method changed to non protected

--- django_extra/core/services.py
+++ django_extra/core/services.py
@@ -19,7 +19,7 @@ class BaseService:
         if instance_id:
             self.instance = get_object_or_404(self.model, id=instance_id)
 
-    def _get_cache_key(self):
+    def get_cache_key(self):
         return f"{app_settings.SERVICE_NAME}:{self.instance.__class__.__name__.lower()}:{self.instance.id}"
 
     def create(self, data):
@@ -29,14 +29,14 @@ class BaseService:
             ser.save()
             self.instance = ser.instance
             if app_settings.USE_SERVICE_CACHE and self.cache_serializer:
-                CustomCache(self._get_cache_key()).set(
+                CustomCache(self.get_cache_key()).set(
                     self.cache_serializer(self.instance).data
                 )
             return self.instance
 
     def delete(self):
         if app_settings.USE_SERVICE_CACHE and self.cache_serializer:
-            CustomCache(self._get_cache_key()).delete()
+            CustomCache(self.get_cache_key()).delete()
         self.instance.delete()
 
     def update(self, data, partial=True):
@@ -46,7 +46,7 @@ class BaseService:
             ser.save()
             self.instance = ser.instance
             if app_settings.USE_SERVICE_CACHE and self.cache_serializer:
-                CustomCache(self._get_cache_key()).set(
+                CustomCache(self.get_cache_key()).set(
                     self.cache_serializer(self.instance).data
                 )
             return self.instance
