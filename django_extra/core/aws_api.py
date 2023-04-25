b4e1acb7341ba9409c0234449e087aa329392efa
Rishabh Dev <rishabh.gupta@saraloan.in>
2023-04-25 13:13:39 +0530

    ~ audit service

---
+++ django_extra/core/aws_api.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import time
+import boto3
+from django_extra.settings import app_settings
+
+class AWSApi:
+    def __init__(self, service_name):
+        self.client = boto3.client(
+            service_name,
+            region_name=os.getenv("AWS_REGION", "ap-south-1"),
+        )
+
+    def send_message(self, inserted, request=dict, remark="", action="", data=""):
+        body = {
+            "is_critical": True,
+            "timestamp":time.time(),
+            "action": action,
+            "data": data,
+            "meta": {
+                "entity_id": inserted.id,
+                "entity_name": inserted.__class__.__name__,
+                "service_name": app_settings.SERVICE_NAME,
+            },
+            "remark": remark,
+            "user": {
+                "user_id": request.auth_user.get("user_id")
+                if hasattr(request, "auth_user")
+                else None
+            },
+        }
+        return self.client.send_message(
+            QueueUrl=app_settings.CONFIG_SECRET.get(
+                "audit_queue_uri",
+                "https://sqs.ap-south-1.amazonaws.com/659590398921/dev-crego-audit-log-queue.fifo",
+            ),
+            MessageBody=json.dumps(body),
+            MessageGroupId=app_settings.SERVICE_NAME,
+        )
--- django_extra/core/services.py
+++ django_extra/core/services.py
@@ -3,26 +3,26 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import transaction
 from django.shortcuts import get_object_or_404
-
 from django_extra.core.cache import CustomCache
 from django_extra.settings import app_settings
-
 # pylint: disable=not-callable
+from django_extra.core.aws_api import AWSApi
 
 
 class BaseService:
     serializer = None
     cache_serializer = None
     model = None
-
+    audit_enable = None
+    
     def __init__(self, instance_id=None):
         if instance_id:
             self.instance = get_object_or_404(self.model, id=instance_id)
-
+    
     def get_cache_key(self):
         return f"{app_settings.SERVICE_NAME}:{self.instance.__class__.__name__.lower()}:{self.instance.id}"
-
-    def create(self, data):
+    
+    def create(self, data, request=dict):
         with transaction.atomic():
             ser = self.serializer(data=data)
             ser.is_valid(raise_exception=True)
@@ -32,14 +32,31 @@ class BaseService:
                 CustomCache(self.get_cache_key()).set(
                     self.cache_serializer(self.instance).data
                 )
+            
+            if self.audit_enable:
+                sqs = AWSApi("sqs")
+                sqs.send_message(
+                    self.instance,
+                    data=data,
+                    request=request,
+                    remark=f"create {self.instance.__class__.__name__}",
+                    action="create")
             return self.instance
-
-    def delete(self):
+    
+    def delete(self, request=dict):
         if app_settings.USE_SERVICE_CACHE and self.cache_serializer:
             CustomCache(self.get_cache_key()).delete()
         self.instance.delete()
-
-    def update(self, data, partial=True):
+        if self.audit_enable:
+            sqs = AWSApi("sqs")
+            sqs.send_message(
+                self.instance,
+                data="",
+                request=request,
+                remark=f"delete {self.instance.__class__.__name__}",
+                action="delete")
+    
+    def update(self, data, request=dict, partial=True):
         with transaction.atomic():
             ser = self.serializer(self.instance, data, partial=partial)
             ser.is_valid(raise_exception=True)
@@ -49,4 +66,12 @@ class BaseService:
                 CustomCache(self.get_cache_key()).set(
                     self.cache_serializer(self.instance).data
                 )
+            if self.audit_enable:
+                sqs = AWSApi("sqs")
+                sqs.send_message(
+                    self.instance,
+                    data="",
+                    request=request,
+                    remark=f"update {self.instance.__class__.__name__}",
+                    action="update")
             return self.instance
