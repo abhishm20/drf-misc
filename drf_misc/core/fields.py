710d36b5c9317101b5c4b511b4e387a6205b03b4
Abhishek Sharma <abhishm20@gmail.com>
2023-07-03 17:50:09 +0530

    default value for EpochField issue fixes

--- drf_misc/core/fields.py
+++ drf_misc/core/fields.py
@@ -25,7 +25,7 @@ class EpochField(models.CharField):  # Store as CharField for simplicity
         self.update_now = kwargs.pop("update_now", False)
         kwargs["max_length"] = kwargs.pop("max_length", 20)
         if self.default_current_time:
-            kwargs["default"] = str(time.time())
+            kwargs["default"] = time.time
         if self.nullable:
             kwargs["null"] = True
             kwargs["blank"] = True
