80bee77a33909439fdff57c250eefa301628db7a
Hardik Aggarwal <hardikmangla195@gmail.com>
2023-07-25 12:27:12 +0530

    Cache settings update

--- drf_misc/settings.py
+++ drf_misc/settings.py
@@ -3,28 +3,28 @@ from django.conf import settings
 
 # pylint: disable=invalid-name
 
+temp_app_settings = getattr(settings, "DRF_MISC_SETTINGS", {})
+assert temp_app_settings.get("service_name") != None
+
 
 class AppSettings:
-    def __init__(self):
-        self.app_settings = getattr(settings, "DRF_MISC_SETTINGS", {})
-
     @property
     def service_name(self):
         """Control how many times a task will be attempted."""
-        return self.app_settings.get("service_name", "django_extra")
+        return temp_app_settings.get("service_name", "django_extra")
 
     @property
     def audit_queue_url(self):
         """Control how many times a task will be attempted."""
-        return self.app_settings.get("audit_queue_url", {})
+        return temp_app_settings.get("audit_queue_url", {})
 
     @property
     def use_service_cache(self):
-        return self.app_settings.get("use_service_cache", False)
+        return temp_app_settings.get("use_service_cache", False)
 
     @property
     def auth_check_disabled_paths(self):
-        return self.app_settings.get("auth_check_disabled_paths", [])
+        return temp_app_settings.get("auth_check_disabled_paths", [])
 
 
 app_settings = AppSettings()
