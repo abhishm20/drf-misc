3c22684e27df51276b2372eb7a07f5969bff675f
Abhishek Sharma <abhishm20@gmail.com>
2024-06-12 13:11:01 +0530

    id default value set + utility functions added

--- drf_misc/utility/misc.py
+++ drf_misc/utility/misc.py
@@ -166,3 +166,17 @@ def decode_jwt(token):
         if app_settings.app_logger:
             app_settings.app_logger.exception(error)
         return False, "Authentication failed"
+
+
+def get_headers(request):
+    import re
+
+    regex_http_ = re.compile(r"^HTTP_.+$")
+    regex_content_type = re.compile(r"^CONTENT_TYPE$")
+    regex_content_length = re.compile(r"^CONTENT_LENGTH$")
+
+    request_headers = {}
+    for header in request.META:
+        if regex_http_.match(header) or regex_content_type.match(header) or regex_content_length.match(header):
+            request_headers[header] = request.META[header]
+    return request_headers
