9ac90ff0063cc46c8ce36ece1162f167be68f283
Abhishek Sharma <abhishm20@gmail.com>
2024-06-12 22:53:33 +0530

    get_headers changes

--- drf_misc/utility/misc.py
+++ drf_misc/utility/misc.py
@@ -169,14 +169,4 @@ def decode_jwt(token):
 
 
 def get_headers(request):
-    import re
-
-    regex_http_ = re.compile(r"^HTTP_.+$")
-    regex_content_type = re.compile(r"^CONTENT_TYPE$")
-    regex_content_length = re.compile(r"^CONTENT_LENGTH$")
-
-    request_headers = {}
-    for header in request.META:
-        if regex_http_.match(header) or regex_content_type.match(header) or regex_content_length.match(header):
-            request_headers[header] = request.META[header]
-    return request_headers
+    return {"Authorization": request.META.get("HTTP_AUTHORIZATION")}
