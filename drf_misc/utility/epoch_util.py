4b6ac220ab3834a5d217775faabb1bc64ec33731
Abhishek Sharma <abhishm20@gmail.com>
2024-07-25 12:53:57 +0530

    epoch utlity added more functions

--- drf_misc/utility/epoch_util.py
+++ drf_misc/utility/epoch_util.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+
+from core.api_exceptions import BadRequest
 from dateutil.relativedelta import relativedelta
 
-from datetime import datetime, timedelta
 
 def unix_to_dt(unix):
     # uts can be 1686868399 or 1686868399000 (millisecond) or 1686868399.000 or 1686868399000000 (microseconds) [Not supported for now]
@@ -59,17 +61,20 @@ def sod(unix):
 def eod(unix):
     return dt_to_unix(unix_to_dt(unix).replace(hour=23, minute=59, second=59, microsecond=999999))
 
+
 def eow(unix):
     dt = unix_to_dt(unix)
     end_of_week = dt + timedelta(days=(6 - dt.weekday()))
     end_of_week = end_of_week.replace(hour=23, minute=59, second=59, microsecond=999999)
     return dt_to_unix(end_of_week)
 
+
 def sow(unix):
     dt = unix_to_dt(unix)
     start_of_week = dt - timedelta(days=dt.weekday())
     start_of_week = start_of_week.replace(hour=0, minute=0, second=0, microsecond=0)
     return dt_to_unix(start_of_week)
+
 
 def eom(unix):
     dt = unix_to_dt(unix)
@@ -93,7 +98,14 @@ def get_date_array(start, count, asc=True, period="days"):
     date_iter = []
     temp = sod(start)
     for _ in range(count):
-        date_iter.append((temp, eod(temp)))
+        if period == "days":
+            date_iter.append((sod(temp), eod(temp)))
+        elif period == "weeks":
+            date_iter.append((sow(temp), eow(temp)))
+        elif period == "months":
+            date_iter.append((som(temp), eom(temp)))
+        else:
+            raise BadRequest({"message": "Invalid period"})
         if asc:
             temp = delta_time(temp, {period: 1})
         else:
